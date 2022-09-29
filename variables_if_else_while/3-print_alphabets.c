c19203dc270a7cd0961346a01784ddc7b23d1972
gabrielblasina <5722@holbertonshool.com>
2022-09-29 06:26:19 -0700

    task-3

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char abc;
+
+	for (abc = 'a'; abc <= 'z'; abc++)
+	{
+		putchar (abc);
+
+	}
+	if (abc >= 'z')
+	{
+		for (abc = 'A' ; abc <= 'Z' ; abc++)
+		{
+			putchar (abc);
+		}
+		putchar ('\n');
+		return (0);
+	}
+}
