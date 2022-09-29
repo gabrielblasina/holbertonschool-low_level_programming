95889d33a753bfdceb159d29f7e713d33f6f8b69
gabrielblasina <5722@holbertonshool.com>
2022-09-29 08:01:16 -0700

    task-6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int n2c;
+
+	for (n2c = 48 ; n2c <= 57 ; n2c++)
+	{
+		putchar(n2c);
+	}
+	putchar('\n');
+	return (0);
+}
