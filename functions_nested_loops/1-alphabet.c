d44391f21a62fc39d2cfd47b4c186afcdead8647
gabrielblasina <5722@holbertonshool.com>
2022-09-30 09:10:56 -0700

    task-1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *print_alphabet - Entry point
+ *Return: 0 (Success)
+ */
+void print_alphabet(void)
+{
+	char a_z;
+
+	for (a_z = 'a' ; a_z <= 'z' ; a_z++)
+	{
+		_putchar(a_z);
+	}
+		_putchar(a_z);
+}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
 int _putchar(char c);
+void print_alphabet(void);
 
 
