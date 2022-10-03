8c60461048032834ea7aa76ff4e120b5ca1a835e
gabrielblasina <5722@holbertonshool.com>
2022-10-03 10:40:45 -0700

    task-9

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -2,32 +2,38 @@
 /**
 *times_table - entry point
 *Return: void
-*@r: variable
-*@i: variable
 */
 void times_table(void)
 {
 	int r;
 	int i;
 	int result;
-	
+
 	for (r = 0 ; r <= 9 ; r++)
 	{
-		for (i = 0 ; i <= 9 ; i++)
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+
+		for (i = 1 ; i <= 9 ; i++)
 		{
-			result = (r * i);
-				
-			if (result > 9)
+			result = r * i;
+			if (((result / 10) + '0') == '0')
+			{
+				_putchar(' ');
+				_putchar(result + '0');
+			}
+			else
 			{
 				_putchar((result / 10) + '0');
 				_putchar((result % 10) + '0');
 			}
-			else if (i != 9)
+			if (i < 9)
 			{
 				_putchar(',');
 				_putchar(' ');
 			}
 		}
-		_putchar('\n');
+		_putchar(10);
 	}
 }
