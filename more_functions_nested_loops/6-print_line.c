32b81f7df74134611ba4d258ecb785fc31da408c
gabrielblasina <5722@holbertonshool.com>
2022-10-04 11:56:27 -0700

    task-6

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -10,14 +10,14 @@ void print_line(int n)
 
 	if (n > 0)
 	{
-		for (x = 0 ; x <= n ; x++)
+		for (x = 0 ; x < n ; x++)
 		{
 			_putchar('_');
 		}
+		_putchar(10);
 	}
 	else if (n <= 0)
 	{
 		_putchar(10);
 	}
-	_putchar(10);
 }
