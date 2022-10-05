656be1a58f92bb19129fb33310f23c22a1dc2b3b
gabrielblasina <5722@holbertonshool.com>
2022-10-05 07:14:06 -0700

    task-10

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * print_triangle - entry point
+ * Return: void
+ * @size: variable
+*/
+void print_triangle(int size)
+{
+	int x;
+	int y;
+
+	if (size > 0)
+	{
+		for (x = 0 ; x < size ; x++)
+		{
+			for (y = size ; y >= 0 ; y--)
+			{
+				if (y > x)
+				{
+					 _putchar(' ');
+				}
+				else if (y <= x)
+				{
+					_putchar('#');
+				}
+			}
+			_putchar(10);
+		}
+	}
+	else if (size <= 0)
+	{
+		_putchar(10);
+	}
+}
