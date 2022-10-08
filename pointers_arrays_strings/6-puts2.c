cff634fc53d62ac9f6f2189b7fa99ac83cd59313
gabrielblasina <5722@holbertonshool.com>
2022-10-08 13:13:45 -0700

    fix output

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -4,13 +4,19 @@
  * Return: void
  * @str: variable
 */
+
 void puts2(char *str)
 {
 	int n;
+	int aux;
 
-	for (n = 0 ; str[n] != '\0' ; n += 2)
+	for (n = 0; str[n] != '\0' ; n++)
 	{
-		_putchar(str[n]);
+		aux++;
+	}
+	for (aux = 0 ; str[aux] != '\0' ; aux += 2)
+	{
+		_putchar(str[aux]);
 	}
 	_putchar(10);
 }
