2e399ff3f843d1b1a12d2a87777b10dabf0c4e74
gabrielblasina <5722@holbertonshool.com>
2022-10-11 12:29:44 -0700

    task-4

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+*reverse_array - entry point
+*Return: void
+*@a: pointer
+*@n: variable
+*/
+void reverse_array(int *a, int n)
+{
+	int i;
+	char temp;
+
+	for (i = 0 ; i < (n / 2) ; i++)
+	{
+		temp = a[i];
+		a[i] = a[n - i - 1];
+		a[n - i - 1] = temp;
+	}
+}
