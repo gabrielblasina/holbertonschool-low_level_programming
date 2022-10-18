e800eeee59d2bfb1fa911ad5620250412d9e7eba
gabrielblasina <5722@holbertonshool.com>
2022-10-18 09:39:09 -0700

    task-0

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
+/**
+ * main - entry point
+ * Return: 0 (success)
+ * @argc: atribute
+ * @argv: atribute
+*/
 int main(__attribute__((unused)) int argc, char **argv)
 {
 	printf("%s\n", *argv);
 	return (0);
-}	
+}
