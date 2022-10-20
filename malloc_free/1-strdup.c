06885fbdf34dbfec0f70ecd15bba3a45f535b5a2
gabrielblasina <5722@holbertonshool.com>
2022-10-20 11:23:54 -0700

    task-1

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+/**
+ * _strdup - entry point
+ * Return: NULL (str is NULL) p2str (success)
+ * @str: string
+*/
+char *_strdup(char *str)
+{
+	int i;
+	int x;
+	char *p2str;
+
+	if (str == NULL)
+		return (str);
+
+	for (i = 0 ; str[i] != '\0' ; i++)
+
+	p2str = malloc(i * sizeof(char));
+	for (x = 0 ; str[x] != '\0' ; x++)
+	{
+		if (x < i)
+		{
+			p2str[x] = str[x];
+		}
+	}
+	return (p2str);
+}
