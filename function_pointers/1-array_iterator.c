2fc150b9db69c413051b9a5d74b3397d991d1456
gabrielblasina <5722@holbertonshool.com>
2022-11-01 11:31:03 -0700

    task-1

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+/**
+ * array_iterator - function that executes a function
+ * Return: void
+ * @array: pointer to array of integers
+ * @size: size of the array
+ * @action: pointer to function
+*/
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int cont;
+
+	for (cont = 0 ; cont < size ; cont++)
+	{
+		action(array[cont]);
+	}
+}
