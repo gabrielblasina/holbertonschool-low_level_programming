681790765810807a51b627fa208837e03b4f61dd
gabrielblasina <5722@holbertonshool.com>
2022-11-16 10:26:01 -0800

    task-0

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -8,15 +8,21 @@ size_t print_list(const list_t *h)
 {
 	int counter = 0;
 
-	do {
-		if (h->len && h->str)
-			printf("[%d]", h->len);
-		if (h->str)
-			printf(" %s\n", h->str);
-		else
+	while (h->next)
+	{
+		if (h->str == NULL)
+		{
 			printf("[0] (nil)\n");
+			counter++;
+			h = h->next;
+		}
+		printf("[%d]", h->len);
+		printf(" %s\n", h->str);
 		counter++;
 		h = h->next;
-	} while (h);
+	}
+	printf("[%d]", h->len);
+	printf(" %s\n", h->str);
+	counter++;
 	return (counter);
 }
