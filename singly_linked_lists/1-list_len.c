1ab1fd5d2741fc3b09917d98c7de7ce16b981b77
gabrielblasina <5722@holbertonshool.com>
2022-11-18 04:29:41 -0800

    task-1

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * list_len - counter of elements in a struct
+ * Return: count
+ * @h:pointer to struct
+*/
+size_t list_len(const list_t *h)
+{
+	int count = 0;
+	const list_t *aux;
+	
+	if (h)
+	{
+		for (aux = h ; aux->next ; aux = aux->next)
+		{
+			count++;
+		}
+	}
+	if (aux->next == NULL)
+		count++;
+	return (count);
+}
