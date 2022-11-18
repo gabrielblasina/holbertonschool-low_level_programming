0760df4b59eccb7e5edf873681d0b1f88b1df10b
gabrielblasina <5722@holbertonshool.com>
2022-11-18 10:15:30 -0800

    task-4

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -1,8 +1,8 @@
 #include "lists.h"
 /**
- *
- *
- *
+ * free_list - function to free a sigly linked lists
+ * Return: void
+ * @head: pointer to singly linked lists
 */
 void free_list(list_t *head)
 {
