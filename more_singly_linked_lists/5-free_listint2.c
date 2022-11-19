9012d583e7021450b67343ed0e4c26168a655ccd
gabrielblasina <5722@holbertonshool.com>
2022-11-19 08:44:44 -0800

    task-5

---
+++ more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * free_listint2 - function to free a sigly linked lists
+ * Return: void
+ * @head: pointer to singly linked lists
+*/
+void free_listint2(listint_t **head)
+{
+	listint_t *deleteaux;
+
+	while (*head)
+	{
+		deleteaux = *head;
+		*head = (*head)->next;
+		free(deleteaux);
+	}
+	*head = NULL;
+}
