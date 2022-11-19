2444d6e20a23f45df927601f9bcea5b2b9389649
gabrielblasina <5722@holbertonshool.com>
2022-11-19 06:09:18 -0800

    betty fix

--- more_singly_linked_lists/lists.h
+++ more_singly_linked_lists/lists.h
@@ -4,9 +4,8 @@
 #include <string.h>
 #include <stdio.h>
 /**
- * struct list_s - singly linked list
- * @str: string - (malloc'ed string)
- * @len: length of the string
+ * struct listint_s - singly linked list
+ * @n: integer
  * @next: points to the next node
  *
  * Description: singly linked list node structure
