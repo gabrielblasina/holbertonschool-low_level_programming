a337e22a6b31e0f71e6c14b94c350a85550e4cef
gabrielblasina <5722@holbertonshool.com>
2022-11-20 16:50:47 -0800

    task-0

---
+++ bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * binary_to_uint - function to convert binary to unsigned int
+ * Return: sum 
+ * @b: string of binary numbers
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int len = 0;
+	int i = 0;
+	unsigned int sum = 0;
+
+	if (b == NULL)
+		return (sum);
+	for ( ; b[len] != '\0' ; len++)
+		;
+	len -= 1;
+	while (b[i])
+	{
+		if ((b[i] != '0') && (b[i] != '1'))
+			return (sum);
+		if (b[i] == '1')
+			sum += (1 << len);
+		i++;
+		len--;
+	}
+	return (sum);
+}
---
+++ bit_manipulation/main.h
@@ -0,0 +1,6 @@
+#ifndef main_h
+#define main_h
+#include <stdio.h>
+#include <stdlib.h>
+unsigned int binary_to_uint(const char *b);
+#endif
