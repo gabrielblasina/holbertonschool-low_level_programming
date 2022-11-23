30628dbbb5ea5721be00ee86be4956dda03741b2
gabrielblasina <5722@holbertonshool.com>
2022-11-23 14:16:20 -0800

    task-1

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -10,11 +10,16 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd, wr;
 
-	if (!filename)
+	if (filename == NULL)
 		return (-1);
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd < 0)
 		return (-1);
+	if (text_content == NULL)
+	{
+		close(fd);
+		return (1);
+	}
 	wr = write(fd, text_content, _strlen(text_content));
 	if (wr < 0)
 	{
