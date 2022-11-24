2b73591a0a6ceda53d971f5e98a73bbf0bff3337
gabrielblasina <5722@holbertonshool.com>
2022-11-24 15:12:34 -0800

    task-3

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,5 +1,5 @@
 #include "main.h"
-int error_list(int errnum, char *str, int fd);
+void error_list(int errnum, char *str, int fd);
 /**
  * main - program to copy first file to the second one
  * Return: 0 (if success) 97, 98, 99, 100 (exit values)
@@ -12,7 +12,10 @@ int main(int argc, char **argv)
 	char *buff[1024];
 
 	if (argc > 3)
-		error_list(97, NULL, 0);
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
 	fd2 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fd2 < 0)
 		error_list(99, argv[2], 0);
@@ -25,7 +28,7 @@ int main(int argc, char **argv)
 			error_list(98, argv[1], 0);
 		wr = write(fd2, buff, rd);
 		if (wr < 0)
-			error_list(99, argv[2], 0);
+			error_list(97, argv[2], 0);
 	}
 	close(fd1);
 		if (fd1 < 0)
@@ -42,7 +45,7 @@ int main(int argc, char **argv)
  * @str: name of the file with error
  * @fd: file descriptor
 */
-int error_list(int errnum, char *str, int fd)
+void error_list(int errnum, char *str, int fd)
 {
 	switch (errnum)
 	{
@@ -58,7 +61,5 @@ int error_list(int errnum, char *str, int fd)
 		case 100:
 			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
 			exit(100);
-		default:
-			return (0);
 	}
 }
